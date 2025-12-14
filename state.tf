1c8d5b063f973e8555b5b811ed790b63063bf8c6
VishalSC4 <vishalschavan4@gmail.com>
2025-12-14 16:19:07 +0530

    New Push

--- state.tf
+++ state.tf
@@ -16,6 +16,9 @@ ami = "ami-00ca570c1b6d79f36"
 instance_type = "t2.micro"
 key_name = "Vishal"
 subnet_id = "subnet-0efa95655268209d2"
+tags = {
+  Name = "Used_for_terraform_backend"
+}
 
 lifecycle {
   create_before_destroy = true
